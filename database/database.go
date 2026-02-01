016e048175246d2dd22844d7d45e978ad9b17161
hiraihan <241111075@student.unu-jogja.ac.id>
2026-02-01 18:22:17 +0700

    update layered architecture

---
+++ database/database.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func InitDB(connectionString string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+
+	log.Println("Database connected")
+	return db, nil
+}
